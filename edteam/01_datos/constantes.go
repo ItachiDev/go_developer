2621200ecf59f6d8e35ea0aa573ddd9930d81a0a
itachidev <cristhofer.ordaz@outlook.com>
2018-02-08 06:43:37 -0600

    Clase 2 Constantes
    Declarar y usar una constante en go al igual que el standar de
    comentación de codigo para el lenguaje go

---
+++ edteam/01_datos/constantes.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/**
+	 * las variables se declaran con la palabra reservada const
+	 * y su valor debe ser inmediatamente asignado
+	 */
+
+	// Documentar el codigo con comentarios en linea
+	// Segun un standar de Go
+	// Los comentarios en bloque son para comentar codigo
+
+	const nombre = "Cristhofer Ordaz"
+	fmt.Println(nombre)
+}
