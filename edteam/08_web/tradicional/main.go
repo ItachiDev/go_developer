52fb4aa0a1de67d3e383ba29a90d30488a2b5d52
itachidev <cristhofer.ordaz@outlook.com>
2018-02-10 08:11:26 -0600

    Clase 9 Servidor Tradicional
    Configurar un servidor tradicional con go

---
+++ edteam/08_web/tradicional/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	/**
+	 * http.Handle("/", http.FileServer(http.Dir("public")))
+	 * log.Println("Ejecutando server en http://localhost:8080")
+	 * log.Println(http.ListenAndServe(":8080", nil))
+	 */
+
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("public"))
+	mux.Handle("/", fs)
+	log.Println("Ejecutando server en http://localhost:8080")
+	log.Println(http.ListenAndServe(":8080", mux))
+}
