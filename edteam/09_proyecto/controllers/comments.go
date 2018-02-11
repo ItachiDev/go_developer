4c41c1c3c43265a5de7de3dda0064e34a3e84fbb
itachidev <cristhofer.ordaz@outlook.com>
2018-02-11 07:59:03 -0600

    Clase 10 Consultar Comentarios
    funcion para consultar los comentarios y configuración de la ruta que
    haga referencia al controlador

--- edteam/09_proyecto/controllers/comments.go
+++ edteam/09_proyecto/controllers/comments.go
@@ -39,3 +39,57 @@ func CommentCreate(w http.ResponseWriter, r *http.Request) {
 	m.Message = "Comentario creado con exito"
 	commons.DisplayMessage(w, m)
 }
+
+
+// CommentGetAll contiene todos los comentarios
+func CommentGetAll(w http.ResponseWriter, r *http.Request) {
+	comments := []models.Comment{}
+	m := models.Message{}
+	user := models.User{}
+	vote := models.Vote{}
+
+	r.Context().Value(&user)
+	vars := r.URL.Query()
+
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	cComment := db.Where("parent_id = 0")
+	if order, ok := vars["order"]; ok {
+		if order[0] == "votes" {
+			cComment = cComment.Order("votes desc, create_at desc")
+		}
+	} else {
+		if idlimit, ok := vars["idlimit"]; ok {
+			registerByPage := 30
+			offset, err := strconv.Atoi(idlimit[0])
+
+			if err != nil {
+				log.Println("Error:", err)
+			}
+
+			cComment = cComment.Where("id BETWEEN ? AND ?", offset - registerByPage, offset)
+		}
+		cComment = cComment.Order("id desc")
+	}
+
+	cComment.Find(&comments)
+	j, err := json.Marshal(comments)
+	
+	if err != nil {
+		m.Code = http.StatusInternalServerError
+		m.Message = "Error al convertir los comentarios en json"
+		commons.DisplayMessage(w, m)
+		return
+	}
+
+	if len(comments) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	} else {
+		m.Code = http.StatusNoContent
+		m.Message = "No se encontraron comentarios"
+		commons.DisplayMessage(w, m)
+	}
+}
--- edteam/09_proyecto/routes/comments.go
+++ edteam/09_proyecto/routes/comments.go
@@ -10,6 +10,7 @@ func SendCommentRouter(router *mux.Router) {
 	prefix := "/api/comments"
 	subRouter := mux.NewRouter().PathPrefix(prefix).Subrouter().StrictSlash(true)
 	subRouter.HandleFunc("/", controllers.CommentCreate).Methods("POST")
+	subRouter.HandleFunc("/", controllers.CommentGetAll).Methods("GET")
 
 	router.PathPrefix(prefix).Handler(
 		negroni.New(
