ca941fb856fcb4724330166a1ccc383f51861353
itachidev <cristhofer.ordaz@outlook.com>
2018-02-10 23:34:29 -0600

    Clase 10 User Controller
    Se creo el archivo controlador para usuarios con la funcion de login y
    crear usuario

---
+++ edteam/09_proyecto/controllers/user.go
@@ -0,0 +1,97 @@
+package controllers
+
+import (
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go_developer/edteam/09_proyecto/commons"
+	"github.com/go_developer/edteam/09_proyecto/configuration"
+	"github.com/go_developer/edteam/09_proyecto/models"
+)
+
+// Controlador de Login
+func Login(w http.ResponseWriter, r *http.Request) {
+	user := models.User{}
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error %s\n", err)
+		return
+	}
+
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	c := sha256.Sum256([]byte(user.Password))
+	pwd := base64.URLEncoding.EncodeToString(c[:32])
+
+	db.Where("email = ? and password = ?", user.Email, pwd).First(&user)
+	if user.ID > 0 {
+		user.Password = ""
+		token := commons.GenerateJWT(user)
+
+		j, err := json.Marshal(models.Token{Token: token})
+		if err != nil {
+			log.Fatalf("Error al convertir el token a json: %s", err)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	} else {
+		m := models.Message{
+			Message: "Usuario o Password no válido",
+			Code:    http.StatusUnauthorized,
+		}
+
+		commons.DisplayMessage(w, m)
+	}
+}
+
+// UserCreate permite registrar un Usuario
+func UserCreate(w http.ResponseWriter, r *http.Request) {
+	user := models.User{}
+	m := models.Message{}
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		m.Message = fmt.Sprintf("Error al leer el usuario a registrar: %s", err)
+		m.Code = http.StatusBadRequest
+		commons.DisplayMessage(w, m)
+		return
+	}
+
+	if user.Password != user.ConfirmPassword {
+		m.Message = "Las contraseñas no coinciden"
+		m.Code = http.StatusBadRequest
+		commons.DisplayMessage(w, m)
+		return
+	}
+
+	c := sha256.Sum256([]byte(user.Password))
+	pwd := fmt.Sprintf("%x", c)
+	user.Password = pwd
+
+	picmd5 := md5.Sum([]byte(user.Email))
+	picstr := fmt.Sprintf("%x", picmd5)
+	user.Picture = "https://gravatar.com/avatar/" + picstr + "?s=100"
+
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	err = db.Create(&user).Error
+	if err != nil {
+		m.Message = fmt.Sprintf("Error al crear el registro: %s", err)
+		m.Code = http.StatusBadRequest
+		commons.DisplayMessage(w, m)
+		return
+	}
+
+	m.Message = "Usuario creado con exito"
+	m.Code = http.StatusCreated
+	commons.DisplayMessage(w, m)
+}
